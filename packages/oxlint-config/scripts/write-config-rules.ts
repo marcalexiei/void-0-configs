97a6c2b0057bb00c0c4dbbc8c1ab93d5a81aa85a
Marco Pasqualetti <marco.pasqualetti@live.com>
2026-03-20 10:19:30 +0100

    chore(oxlint-config): improve tooling

---
+++ packages/oxlint-config/scripts/write-config-rules.ts
@@ -0,0 +1,50 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import process from 'node:process';
+
+// @ts-expect-error
+import { log, OXLINT_RULES } from './_utils.ts';
+
+/**
+ * ```shell
+ * pnpm --filter="@marcalexiei/oxlint-config" run rules:write unicorn
+ * ```
+ */
+
+const [, , pluginScope] = process.argv;
+
+const pluginScopes = new Set(OXLINT_RULES.map((it) => it.scope));
+
+console.info(process.argv);
+
+if (!pluginScope) {
+  log('error', 'missing plugin scope');
+  process.exit(1);
+}
+
+if (pluginScopes.has(pluginScope) === false) {
+  log(
+    'error',
+    'invalid plugin scope: ',
+    pluginScope,
+    '\n',
+    'allowed values:',
+    Array.from(pluginScopes).join(', '),
+  );
+  process.exit(1);
+}
+
+const configRules = OXLINT_RULES.filter((it) => it.scope === pluginScope);
+
+const rules: Record<string, string> = {};
+
+for (const rule of configRules) {
+  const ruleName = `${pluginScope}/${rule.value}`;
+  rules[ruleName] = 'error';
+}
+
+const outputFilePath = path.join(import.meta.dirname, '..', 'generated-rules.json');
+
+fs.writeFileSync(outputFilePath, JSON.stringify(rules, null, 2));
+
+log('success', `${pluginScope} rules available in ${outputFilePath}`);
